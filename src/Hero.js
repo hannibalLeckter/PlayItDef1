bcf322b2d97d635b738c5faa657ffa364421d3d9
Ivan Lopez Toledo <ivlopeztol@gmail.com>
2021-06-01 19:52:33 +0200

    f

--- src/Hero.js
+++ src/Hero.js
@@ -1,11 +1,30 @@
- import React from 'react';
+ import React, {useState} from 'react';
  import fire from './fire';
  import Music from './Music';
  import Crud from './crud';
  import Side from './Sidebar';
+ import { FaPlay,FaTrashAlt,FaEdit,FaUserAlt } from "react-icons/fa";
+ import { MdSettings } from "react-icons/md";
+ import { ImExit } from "react-icons/im";
  import "./hero.css";
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ import {Dropdown, DropdownItem, DropdownMenu,DropdownToggle } from 'reactstrap';
+
+
+
 
  const Hero = (props) =>{
+
+    const mystyle = {
+        padding: "10px",
+        fontFamily: "Arial",
+        width:"100%"
+      };
+
+    const [dropdown, setDropdown]= useState(false);
+    const abrirCerrarDropdown=()=>{
+        setDropdown(!dropdown);
+    }
 
     const CurrentUser = fire.auth().currentUser;
 
@@ -28,13 +47,32 @@ console.log(res2);
          <section className="hero">
              <nav className="nav">
                  
+        
+
                  <h2>Bienvenido {res2}</h2>
-                 <button onClick={props.handleLogout}>Cerrar Sesion</button>
+
+                 <Dropdown isOpen={dropdown} toggle={abrirCerrarDropdown} style={{ position: 'relative', zIndex: '100' }} direction="left" >
+                     <DropdownToggle >
+                        <MdSettings/>
+                     </DropdownToggle>
+
+                     <DropdownMenu >
+                     <DropdownItem header style={mystyle}>Opciones</DropdownItem>
+                     <DropdownItem divider style={mystyle}></DropdownItem>
+                         <DropdownItem style={mystyle}><FaUserAlt/> Ver perfil</DropdownItem>
+                         <DropdownItem style={mystyle} onClick={props.handleLogout}><ImExit/> Cerrar Sesion</DropdownItem>
+                     </DropdownMenu>
+                 </Dropdown>
+                 
+                
+                 
              </nav>
-              <Side/> 
+             
+             
+            <Side/> 
              <Music/>
-            
              <Crud/>
+             
              
              
          </section>
